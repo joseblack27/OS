6e3982a7e710aa7b6f541490e60ca9225831242e
joseblack27 <josedvf27@gmail.com>
2026-01-15 23:15:46 -0500

    refactorizacion de los items de lista

--- scenes/ui/commons/button_list_option_event/ButtonListOptionEvent.gd
+++ scenes/ui/commons/button_list_option_event/ButtonListOptionEvent.gd
@@ -1,24 +1,20 @@
-extends Button
+extends ButtonListItem
 class_name ButtonListOptionEvent
-
-signal button_clicked(data_resource: EventData)
-
-@onready var labels: Array[Label] = [
-	$MarginContainer/VBoxContainer/HBoxContainer2/TitleLabel, 
-	$MarginContainer/VBoxContainer/HBoxContainer/SubtitleLabel, 
-	$MarginContainer/VBoxContainer/HBoxContainer/DateLabel
-]
 
 @onready var title_label: Label = $MarginContainer/VBoxContainer/HBoxContainer2/TitleLabel
 @onready var date_label: Label = $MarginContainer/VBoxContainer/HBoxContainer/DateLabel
 @onready var subtitle_label: Label = $MarginContainer/VBoxContainer/HBoxContainer/SubtitleLabel
 @onready var texture_rect: TextureRect = $MarginContainer/VBoxContainer/HBoxContainer2/TextureRect
 
-@export var resource: EventData
-
-var _original_stylebox: StyleBoxFlat
-var _pressed_stylebox: StyleBoxFlat
-var presionado: bool = false
+func _ready():
+	
+	labels = [
+		$MarginContainer/VBoxContainer/HBoxContainer2/TitleLabel, 
+		$MarginContainer/VBoxContainer/HBoxContainer/SubtitleLabel, 
+		$MarginContainer/VBoxContainer/HBoxContainer/DateLabel
+	]
+	
+	super._ready()
 
 func setup(data: EventData):
 	resource = data
@@ -74,75 +70,3 @@ func refresh():
 			modulate = Color(0.6, 1, 0.6)
 		Enums.Event.Status.COMPLETED:
 			modulate = Color(0.5, 0.5, 0.5)
-
-
-
-func _ready():
-	toggled.connect(_on_toggled)
-	button_down.connect(_on_button_down)
-	button_up.connect(_on_button_up)
-	mouse_exited.connect(_on_mouse_exited)
-	
-	_pressed_stylebox = StyleBoxFlat.new()
-	_pressed_stylebox.bg_color = Color.WHITE
-	_pressed_stylebox.set_border_width_all(1)
-	_pressed_stylebox.border_color = Color(0,0,0)
-	
-	_original_stylebox = StyleBoxFlat.new()
-	_original_stylebox.bg_color = Color.BLACK
-	_original_stylebox.set_border_width_all(1)
-	_original_stylebox.border_color = Color(0.267, 0.267, 0.267)
-	
-	label_font_white()
-	background_black()
-
-func _on_toggled(_pressed: bool) -> void:
-	if _pressed:
-		label_font_black()
-		background_white()
-		button_clicked.emit(resource)
-	else:
-		label_font_white()
-		background_black()
-
-func _on_button_down():
-	presionado = true
-	if button_pressed == true:
-		label_font_black()
-		background_white()
-	else:
-		label_font_white()
-		background_black()
-
-func _on_button_up():
-	if presionado == true:
-		label_font_black()
-		background_white()
-		presionado = false
-
-func _on_mouse_exited():
-	if presionado == true:
-		if button_pressed == true:
-			label_font_black()
-			background_white()
-		else:
-			label_font_white()
-			background_black()
-		presionado = false
-
-
-func label_font_black():
-	for label in labels:
-		label.add_theme_color_override("font_color", Color.BLACK)
-
-func label_font_white():
-	for label in labels:
-		label.add_theme_color_override("font_color", Color.WHITE)
-
-func background_white():
-	add_theme_stylebox_override("pressed", _pressed_stylebox)
-	add_theme_stylebox_override("focus", _pressed_stylebox)
-
-func background_black():
-	add_theme_stylebox_override("pressed", _original_stylebox)
-	add_theme_stylebox_override("focus", _original_stylebox)
